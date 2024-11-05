f71df0b4f3e547bb7b1d8a5b9fb5ef6630b5cfa1
Sholom Konstantin <sholom1998@icloud.com>
2024-11-05 19:36:58 +0100

    app history

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,32 +1,39 @@
 import PropTypes from 'prop-types';
-import TransactionHistoryItem from './TransactionHistoryItem'
+import TransactionHistoryItem from './TransactionHistoryItem';
 import css from './TransactionHistory.module.css';
 
-const TransactionHistory = ({items}) => {
+const TransactionHistory = ({ items }) => {
     return (
         <table className={css.transtable}>
-          <thead>
-            <tr>
-              <th>Type</th>
-              <th>Amount</th>
-              <th>Currency</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map(({ id, type, amount, currency }) => {
-                return (
-                <tr key={id}>
-                    <TransactionHistoryItem type={type} amount={Number(amount)} currency={currency} />
+            <thead>
+                <tr>
+                    <th>Type</th>
+                    <th>Amount</th>
+                    <th>Currency</th>
                 </tr>
-                );
-            })}
-          </tbody>
+            </thead>
+            <tbody>
+                {items.map(({ id, type, amount, currency }) => (
+                    <tr key={id}>
+                        <TransactionHistoryItem 
+                            type={type} 
+                            amount={Number(amount)} 
+                            currency={currency} 
+                        />
+                    </tr>
+                ))}
+            </tbody>
         </table>
     );
 }
 
 TransactionHistory.propTypes = {
-    items: PropTypes.array,
+    items: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        amount: PropTypes.number.isRequired,
+        currency: PropTypes.string.isRequired,
+    })).isRequired,
 };
 
 export default TransactionHistory;
--- src/components/TransactionHistory/TransactionHistoryItem.jsx
+++ src/components/TransactionHistory/TransactionHistoryItem.jsx
@@ -1,15 +1,16 @@
 import PropTypes from 'prop-types';
 import css from './TransactionHistoryItem.module.css';
 
-const TransactionHistoryItem = ({type, amount, currency}) => {
+const TransactionHistoryItem = ({ type, amount, currency }) => {
+    // Форматування суми до двох десяткових знаків
     amount = amount.toFixed(2);
     
     return (
         <>
-        <td className={css.transtype}>{type}</td>
-        <td className={css.transamount}>{amount}</td>
-        <td className={css.transcurrency}>{currency}</td>
-      </>
+            <td className={css.transtype}>{type}</td>
+            <td className={css.transamount}>{amount}</td>
+            <td className={css.transcurrency}>{currency}</td>
+        </>
     );
 }
 
